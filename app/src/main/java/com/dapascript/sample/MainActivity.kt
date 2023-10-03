9f3b33f8e9afdce4f271bb7cb4c1a690b8d444b6
Luthfi Daffa Prabowo <daffaprabowo5@gmail.com>
2023-10-03 12:54:05 +0700

    setup navigation component

--- app/src/main/java/com/dapascript/sample/MainActivity.kt
+++ app/src/main/java/com/dapascript/sample/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.dapascript.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.dapascript.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.fragment_container_view
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/dapascript/sample/presentation/ui/ShopFragment.kt
@@ -0,0 +1,7 @@
+package com.dapascript.sample.presentation.ui
+
+import androidx.fragment.app.Fragment
+
+class ShopFragment : Fragment() {
+
+}
